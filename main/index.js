13fb152effeaaa38ca18e5b618e4ab7c397d7024
Braxton Coats <coatsbr@appstate.edu>
2018-09-11 16:18:17 -0400

    Created call forwarding/masking app

--- main/index.js
+++ main/index.js
@@ -13,31 +13,95 @@ var client = new Bandwidth({
 app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 3000));
 
-// Creates a multimedia message sent to numbers
- var sendMessage = function(params){
-  client.Message.send({
-    //returns a promise
-    from : params.from, //your bandwidth number
-    to   : params.to, //number to send to
-    text : "This is what I did today :).....dont respond to this",
-    media: "https://s3.amazonaws.com/bwdemos/logo.png"
-  })
+http.listen(app.get('port'), function(){
+    console.log('listening on *: ' + app.get('port'));
+});
 
-  //calls back the message id number and catches any errors
-  .then(function(message){
-    console.log(message);
-    return client.Message.get(message.id)
-    //access ID from json can also get to and from
-  })
-  // catches any errors
-  .catch(function(err){
-    console.log(err)
-  });
-}
+const getBaseUrlFromReq = (req) => {
+    return 'http://' + req.hostname;
+};
+app.post("/", function (req, res) {
+    console.log(req);
+    res.send("Bandwdith_Forward_A_Call");
+});
+
+let FromBWnumber = "9192457257";
+let toNumber = "9802416513";
+
+//OUTBOUND CALLS
+app.post('/out-call', function (req, res) {
+    var this_url2 = getBaseUrlFromReq(req);
+    if (req.body.eventType === 'answer') {                    //Upon the to-caller answering, bridge the two calls
+        console.log("Incoming CallId: " + req.body.tag);
+        console.log("Outgoing CallId: " + req.body.callId);
+        console.log(req);
+        client.Bridge.create({
+            bridgeAudio: true,
+            callIds : [req.body.tag, req.body.callId]
+        })
+        .then(function (bridge) {
+            console.log("BridgeId: " + bridge.id);
+            console.log("---Call has been bridged----");
+        })
+        .catch(function(err){
+            console.log(err);
+            console.log("----Could not bridge the call----");
+        });
+    }
+    else if (req.body.eventType === "hangup"){
+        console.log(req.body);
+        console.log("----Your call has hungup----");
+    }
+    else{
+        console.log(req.body);
+    }
+});
+
+//INBOUND CALLS
+app.post('/in-call', function (req, res) {             //When someone calls the BW number, create call to the to-caller
+    if (req.body.eventType === "incomingcall"){
+        console.log("Incoming callId: " + req.body.callId);
+        var this_url1 = getBaseUrlFromReq(req);
+        createCallWithCallback(req.body.to, this_url1, req.body.callId);
+    }
+    else{
+        console.log(req.body);
+    }
+});
+
+//Method to create outbound call with '/out-call' callback url, tag used to store inbound callId
+var createCallWithCallback = function(FromBWnumber, this_url, inbound_callid){
+    return client.Call.create({
+        from: FromBWnumber,
+        to: toNumber,
+        callbackUrl: this_url + '/out-call',
+        tag: inbound_callid
+    })
+    .then(function (call) {
+        console.log("Outgoing call Id: " + call.callId);
+        console.log(call);
+        console.log("----Outbound call has been created----");
+    })
+    .catch(function(err){
+        console.log(err);
+        console.log("---Outbound call could not be created---");
+    })
+};
 
 
-var numbers = {
+
+
+
+/*var numbers = {
         to: "9802416513",
         from: "9192457257" //bandwidth number
 };
-sendMessage(numbers);
+
+client.Call.create({
+    from: "9192457257",
+    to: "9192591068",
+    callbackUrl: "http://1136ad2a.ngrok.io"
+})
+.then(function (id) {
+    console.log();
+})*/
