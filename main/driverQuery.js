08fc6a8d91e47b7d69f3c2d570c7dc99f7004d75
Ubuntu <ubuntu@ip-172-31-39-131.us-east-2.compute.internal>
2018-10-30 18:53:49 +0000

    deleted swap file

--- main/driverQuery.js
+++ main/driverQuery.js
@@ -11,12 +11,13 @@ module.exports = {
 		MongoClient.connect(url, function(err, db) {
   			if (err) throw err;
   			var dbo = db.db("Driver");
-  			var myobj = { name: driverName, number: driverNumber };
+  			var myobj = { name: driverName, number: driverNumber};
   			dbo.collection("drivers").insertOne(myobj, function(err, res) {
     				if (err) throw err;
     				console.log("1 document inserted");
     			db.close();
   			});
 		});
-	}
+	},
+
 }
