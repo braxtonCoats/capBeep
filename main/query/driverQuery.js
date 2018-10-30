d4b91f0058f74726bb16deae59285ae96c137884
Ubuntu <ubuntu@ip-172-31-39-131.us-east-2.compute.internal>
2018-10-30 19:30:15 +0000

    Added method to see if a driver is active

--- main/query/driverQuery.js
+++ main/query/driverQuery.js
@@ -9,41 +9,56 @@ module.exports = {
 	// insert a driver into the database
 	insertDriver: function(driverName, driverNumber) {
 		MongoClient.connect(url, function(err, db) {
-  			if (err) throw err;
-  			var dbo = db.db("Driver");
-  			var myobj = { name: driverName, number: driverNumber};
-  			dbo.collection("drivers").insertOne(myobj, function(err, res) {
-    				if (err) throw err;
-    				console.log("1 document inserted");
-    			db.close();
-  			});
+			if (err) throw err;
+			var dbo = db.db("Driver");
+			var myobj = { name: driverName, number: driverNumber};
+			dbo.collection("drivers").insertOne(myobj, function(err, res) {
+				if (err) throw err;
+				console.log("1 document inserted");
+				db.close();
+			});
 		});
 	},
+
 	popActiveDriver: function(){
 		MongoClient.connect(url, function(err, db) {
-                        if (err) throw err;
-                        var dbo = db.db("Driver");
+			if (err) throw err;
+			var dbo = db.db("Driver");
 
 			// find a driver from the driver collection
-                        var myobj = dbo.collection("drivers").findOne(function(err, res) {
-                                if (err) throw err;
-                                console.log("driver found.");
-                        db.close();
-                        });
-			
+			var myobj = dbo.collection("drivers").findOne(function(err, res) {
+				if (err) throw err;
+				console.log("driver found.");
+				db.close();
+			});
+
 			// add the driver to the active collection
 			dbo.collection("active").insertOne(myobj, function(err, res) {
-                                if (err) throw err;
-                                console.log("driver added to active collection");
-                        db.close();
-                        });
+				if (err) throw err;
+				console.log("driver added to active collection");
+				db.close();
+			});
 
 			// remove the driver from the driver collection
 			dbo.collection("drivers").remove(myobj, function(err, res) {
-                                if (err) throw err;
-                                console.log("driver removed from driver collection");
-                        db.close();
-                        });
-                });
+				if (err) throw err;
+				console.log("driver removed from driver collection");
+				db.close();
+			});
+		});
+	},
+
+	checkActive(): function(num){
+		MongoClient.connect(url, function(err, db) {
+			if (err) throw err;
+			var dbo = db.db("Driver");
+			dbo.collection("active").find({number: num}).toArray(function(err, result) {
+				if (err) throw err;
+				console.log(result);
+				if (result) return true;
+				else return false;
+				db.close();
+			});
+		});
 	}
 }
