e8c53d573d37cb2b545811fbc707a17169ca8113
Ubuntu <ubuntu@ip-172-31-39-131.us-east-2.compute.internal>
2018-10-30 19:12:23 +0000

    added popActive driver method

--- main/query/driverQuery.js
+++ main/query/driverQuery.js
@@ -19,5 +19,31 @@ module.exports = {
   			});
 		});
 	},
+	popActiveDriver: function(){
+		MongoClient.connect(url, function(err, db) {
+                        if (err) throw err;
+                        var dbo = db.db("Driver");
 
+			// find a driver from the driver collection
+                        var myobj = dbo.collection("drivers").findOne(function(err, res) {
+                                if (err) throw err;
+                                console.log("driver found.");
+                        db.close();
+                        });
+			
+			// add the driver to the active collection
+			dbo.collection("active").insertOne(myobj, function(err, res) {
+                                if (err) throw err;
+                                console.log("driver added to active collection");
+                        db.close();
+                        });
+
+			// remove the driver from the driver collection
+			dbo.collection("drivers").remove(myobj, function(err, res) {
+                                if (err) throw err;
+                                console.log("driver removed from driver collection");
+                        db.close();
+                        });
+                });
+	}
 }
